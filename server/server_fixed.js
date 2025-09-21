223f9efd76cf551be1ac40ccfa39eaa349da8a5b
Suraj Giri <155833064+surajixcp@users.noreply.github.com>
2025-09-21 12:38:13 +0530

    helllll

--- server/server_fixed.js
+++ server/server_fixed.js
@@ -71,5 +71,10 @@ app.use("/api/messages", messageRouter)
 // Connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
+if(process.env.NODE_ENV === 'production'){
+
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
+
+} export default server;
+
